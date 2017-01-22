aaecebe1d90c362d8e1185483f5ded143441727b
Meggetto Francesco <fra_megg@hotmail.com>
2017-01-22 18:56:32 +0000

    Add Controller with KeyListener for flipper

---
+++ src/controller/FlipKeyListener.java
@@ -0,0 +1,31 @@
+package src.controller;
+
+import src.model.FlipperModel;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class FlipKeyListener implements KeyListener {
+    private FlipperModel flipper;
+
+    public FlipKeyListener(FlipperModel flipper) {
+        this.flipper = flipper;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == e.VK_SPACE) {
+            flipper.trigger();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == e.VK_SPACE) {
+            flipper.untrigger();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) { }
+}
--- src/view/GizmoBallPrototype.java
+++ src/view/GizmoBallPrototype.java
@@ -1,48 +1,34 @@
 package src.view;
 
+import src.controller.FlipKeyListener;
 import src.model.Model;
 import src.model.FlipperModel;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import javax.swing.Timer;
 
 public class GizmoBallPrototype extends JPanel {
     private FlipperModel flipper;
     private FlipperView flipperView;
+    private static JFrame window;
 
     public static void main(String[] args) {
-        JFrame window = new JFrame();
+        window = new JFrame();
         GizmoBallPrototype p = new GizmoBallPrototype();
         window.add(p);
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(new Dimension(300, 300));
-
-        // TODO: work out how to get the key events forwarded to the src.view.GizmoBallPrototype properly
-        window.addKeyListener(
-                new KeyAdapter() {
-                    public void keyPressed(KeyEvent e) {
-                        if (e.getKeyCode() == e.VK_SPACE) {
-                            p.flipper.trigger();
-                        }
-                    }
-
-                    public void keyReleased(KeyEvent e) {
-                        if (e.getKeyCode() == e.VK_SPACE) {
-                            p.flipper.untrigger();
-                        }
-                    }
-                }
-        );
     }
 
     public GizmoBallPrototype() {
         flipper = new FlipperModel(false);
         flipperView = new FlipperView();
+
+        // TODO: work out how to get the key events forwarded to the src.view.GizmoBallPrototype properly
+        window.addKeyListener(new FlipKeyListener(flipper));
 
         new Timer(1000 / Model.TICKS_PER_SECOND, e -> {
             flipper.tick();
