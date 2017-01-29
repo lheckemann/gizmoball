f80e378f29d599535c7c8688d0c54a06fb65258b
Linus Heckemann <git@sphalerite.org>
2017-01-29 18:14:43 +0000

    Triggering: add controller

---
+++ src/controller/KeyTriggerListener.java
@@ -0,0 +1,24 @@
+package controller;
+
+import model.Model;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyTriggerListener extends KeyAdapter {
+    Model model;
+
+    public KeyTriggerListener(Model model) {
+        this.model = model;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        model.keyPressed(e.getKeyCode());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        model.keyReleased(e.getKeyCode());
+    }
+}
