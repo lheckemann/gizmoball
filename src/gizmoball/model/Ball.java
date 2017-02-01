b0a4e3192554a3918ceb7f18a1777aaf3247d2de
Unai Zalakain <unai@gisa-elkartea.org>
2017-02-01 19:07:47 +0000

    Add boilerplace code for balls

--- src/gizmoball/model/Ball.java
+++ src/gizmoball/model/Ball.java
@@ -1,2 +1,30 @@
+import physics.Vect;
+
 public class Ball {
+    private double x;
+    private double y;
+    private Vect velocity;
+
+    public Ball() {
+        this.x = 0d;
+        this.y = 0d;
+        this.velocity = new Vect(0d, 0d);
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setPosition(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setVelocity(Vect velocity) {
+        this.velocity = velocity;
+    }
 }
