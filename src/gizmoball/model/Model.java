ad9f9a1f040a9b5606c0d0da26ed5ba3614dcd7a
Unai Zalakain <unai@gisa-elkartea.org>
2017-02-03 14:38:41 +0000

    Store the selected coordinates instead of the gizmo or ball

--- src/gizmoball/model/Model.java
+++ src/gizmoball/model/Model.java
@@ -14,8 +14,8 @@ import physics.Vect;
 public class Model implements BuildModel {
     private final double width;
     private final double height;
-    private Gizmo selectedGizmo;
-    private Ball selectedBall;
+    private double selX;
+    private double selY;
     private Map<String, Gizmo> gizmos;
     private Map<String, Ball> balls;
     private Map<Integer, Set<Gizmo>> keyPressMap;
@@ -40,9 +40,21 @@ public class Model implements BuildModel {
         this.wallTriggers = new HashSet<>();
     }
 
+    private Map.Entry<String,Gizmo> getSelectedGizmo() {
+        return this.gizmos.entrySet().stream()
+                                     .filter(e -> e.getValue().contains(x, y))
+                                     .findFirst().orElse(null);
+    }
+
+    private Map.Entry<String,Ball> getSelectedBall() {
+        return this.balls.entrySet().stream()
+                                    .filter(e -> e.getValue().contains(x, y))
+                                    .findFirst().orElse(null);
+    }
+
     public void select(double x, double y) {
-        this.selectedGizmo = this.gizmos.values().stream().filter(g -> g.contains(x, y)).findFirst().orElse(null);
-        this.selectedBall = this.balls.values().stream().filter(b -> b.contains(x, y)).findFirst().orElse(null);
+        this.selX = x;
+        this.selY = y;
     }
 
     public void move(double dX, double dY) {
