889a1b811d451597797ae19daa0e2a6150fa8dbe
William Macdonald <williamacdonald18@googlemail.com>
2017-02-16 23:27:38 +0000

    Complete saving functionality

--- src/gizmoball/controller/SaveListener.java
+++ src/gizmoball/controller/SaveListener.java
@@ -2,17 +2,44 @@ package gizmoball.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import gizmoball.model.Model;
+import gizmoball.model.ReadModel;
 
 public class SaveListener implements ActionListener
 {
-    //private final Model model;
-    //private final GizmoBallView view;
-    public SaveListener() {
-        //this.model = new Model();
-        //this.view = view;
+
+	private Model model;
+    public SaveListener(Model model) {
+    	this.model = model;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.print("save");
+    	try {
+            JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
+            chooser.setFileFilter(new FileNameExtensionFilter("GizmoBall File", "gzm"));
+            if (JFileChooser.APPROVE_OPTION == chooser.showSaveDialog(null)) {
+            	//If the file exists just write to the existing file
+            	if (chooser.getSelectedFile().exists()) {
+            		model.save(new FileOutputStream(chooser.getSelectedFile()));
+            	} //Otherwise add the .gzm extension and write to a new file
+            	else {
+            		model.save(new FileOutputStream(new File(chooser.getSelectedFile().toString() + ".gzm")));
+            	}
+            }
+        } catch (FileNotFoundException fnfe) {
+            JOptionPane.showMessageDialog(null, fnfe.getMessage(), "File not found", JOptionPane.ERROR_MESSAGE);
+            //fnfe.printStackTrace();
+        } 
     }
 }
