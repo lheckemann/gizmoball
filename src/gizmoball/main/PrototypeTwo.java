95f5038c971c2a2babb3be1cfafc84245fe5d6eb
William Macdonald <williamacdonald18@googlemail.com>
2017-02-16 13:49:39 +0000

    Add demo board layout for prototype2

--- src/gizmoball/main/PrototypeTwo.java
+++ src/gizmoball/main/PrototypeTwo.java
@@ -12,6 +12,11 @@ public class PrototypeTwo {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
+            	model.addBall("B0");
+            	model.move(4, 4);
+            	model.addAbsorber("A0", 19, 2);
+            	model.move(0, 18);
+            	model.triggerOnKeyPress(32);
                 GizmoBallView gui = new GizmoBallView(model);
                 JFrame frame = gui.getGUI();
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
