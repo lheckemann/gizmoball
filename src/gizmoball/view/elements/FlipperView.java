73da649542ad06d5948caa8883f1e8616c475628
Linus Heckemann <git@sphalerite.org>
2017-02-16 13:36:54 +0000

    Draw flippers gud

--- src/gizmoball/view/elements/FlipperView.java
+++ src/gizmoball/view/elements/FlipperView.java
@@ -5,61 +5,66 @@ import gizmoball.model.gizmos.GizmoTypeException;
 import gizmoball.model.gizmos.ReadGizmo;
 import gizmoball.model.gizmos.ReadGizmo.GizmoType;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.geom.AffineTransform;
 
 
 public class FlipperView {
-	
-	
+
+
 	public static void paint(Graphics2D graphics, ReadGizmo flipper) {
     	graphics.setColor(Color.ORANGE);
     	AffineTransform originalTransform = graphics.getTransform();
-    	
+
+        // Translate to flipper's top-left corner
+        graphics.translate(flipper.getX() * Model.L_TO_PIXELS, flipper.getY() * Model.L_TO_PIXELS);
+
+        // Rotate (both gizmo rotation and flipper pivot)
     	performFlipperTransformations(graphics, flipper);
-    		
-     	graphics.fillRoundRect(flipper.getX() * Model.L_TO_PIXELS, flipper.getY() * Model.L_TO_PIXELS, flipper.getWidth() * Model.L_TO_PIXELS / 4 , flipper.getHeight() * Model.L_TO_PIXELS, Model.L_TO_PIXELS / 2, Model.L_TO_PIXELS / 2);
+
+        // Draw body
+     	graphics.fillRoundRect(
+     	        0,
+                0,
+                flipper.getWidth() * Model.L_TO_PIXELS / 4,
+                flipper.getHeight() * Model.L_TO_PIXELS,
+                Model.L_TO_PIXELS / 2,
+                Model.L_TO_PIXELS / 2);
+
+        // Draw pivot
     	graphics.setColor(Color.RED);
-    	graphics.translate(0.15 * Model.L_TO_PIXELS, 0.15 * Model.L_TO_PIXELS);
-    	graphics.fillOval(flipper.getX() * Model.L_TO_PIXELS, flipper.getY() * Model.L_TO_PIXELS, Model.L_TO_PIXELS/4, Model.L_TO_PIXELS/4);
-    	
+    	graphics.fillOval(
+    	        Model.L_TO_PIXELS/8,
+                Model.L_TO_PIXELS/8,
+                Model.L_TO_PIXELS/4,
+                Model.L_TO_PIXELS/4);
+
+        // Restore transformation
     	graphics.setTransform(originalTransform);
     }
-	
+
 	private static void performFlipperTransformations(Graphics2D graphics, ReadGizmo flipper) {
-	
-    	int topRightHandX = flipper.getX() + flipper.getWidth();
-    	int bottomRightHandY = flipper.getY() + flipper.getHeight();
-    	int middleX = (flipper.getX() + topRightHandX)/2;
-    	int middleY = (flipper.getY() + bottomRightHandY)/2;
-		
-		graphics.translate(middleX * Model.L_TO_PIXELS, middleY * Model.L_TO_PIXELS);
-    	
+	    int middleX = flipper.getWidth() * Model.L_TO_PIXELS / 2;
+        int middleY = flipper.getHeight() * Model.L_TO_PIXELS / 2;
+		graphics.translate(middleX, middleY);
+
+        // Perform rotation according to gizmo rotation
+        graphics.rotate(flipper.getRotation().getRadiansFromNorth());
+        // Flip horizontally if appropriate
     	if (flipper.getType() == GizmoType.RIGHT_FLIPPER) {
     		graphics.scale(-1, 1);
     	}
-    	switch (flipper.getRotation()) {
-    		case EAST:
-    			graphics.rotate(Math.toRadians(90));
-    			break;
-    		case SOUTH:
-    			graphics.rotate(Math.toRadians(180));
-    			break;
-    		case WEST:
-    			graphics.rotate(Math.toRadians(270));
-    			break;
-    		default:
-    			break;
-    	}
-    	graphics.translate(-0.75 * Model.L_TO_PIXELS, -0.75 * Model.L_TO_PIXELS);
+        graphics.translate(-middleX, -middleY);
+
+        // Translate to pivot point
+    	graphics.translate(0.25 * Model.L_TO_PIXELS, 0.25 * Model.L_TO_PIXELS);
     	try {
 			graphics.rotate(-Math.toRadians(flipper.getPivotAngle()));
 		} catch (GizmoTypeException e1) {
 			assert false : "This Gizmo should have a pivot angle, since it's a flipper";
 		}
-    	graphics.translate(0.75 * Model.L_TO_PIXELS, 0.75 * Model.L_TO_PIXELS);
-    	graphics.translate(-middleX * Model.L_TO_PIXELS, -middleY * Model.L_TO_PIXELS);
+		// Translate back after rotation
+        graphics.translate(-0.25 * Model.L_TO_PIXELS, -0.25 * Model.L_TO_PIXELS);
 	}
 }
 
