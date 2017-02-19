43d6d40b3843b6fc7718e222e920887d647c462c
Linus Heckemann <git@sphalerite.org>
2017-02-19 14:42:39 +0000

    Add circle physics

--- src/gizmoball/model/gizmos/Circle.java
+++ src/gizmoball/model/gizmos/Circle.java
@@ -1,5 +1,10 @@
 package gizmoball.model.gizmos;
 
+import physics.LineSegment;
+
+import java.util.Collections;
+import java.util.Set;
+
 public class Circle extends Gizmo {
     public GizmoType getType() {
         return GizmoType.CIRCLE;
@@ -11,5 +16,16 @@ public class Circle extends Gizmo {
 
     public int getHeight() {
         return 1;
+    }
+
+    private physics.Circle physics = new physics.Circle(0.5, 0.5, 0.5);
+    @Override
+    protected Set<physics.Circle> getBasicCircles() {
+        return Collections.singleton(physics);
+    }
+
+    @Override
+    protected Set<LineSegment> getBasicLineSegments() {
+        return Collections.emptySet();
     }
 }
