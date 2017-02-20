5330101f126e7b4120cc83ec75f2017cff1b05f6
Meggetto Francesco <fra_megg@hotmail.com>
2017-02-20 17:07:26 +0000

    Vertical bar with gravity and frictions text entries

--- src/gizmoball/view/BuildView.java
+++ src/gizmoball/view/BuildView.java
@@ -11,9 +11,10 @@ public class BuildView extends GameView {
     private JPanel buttonsPnl;
 
     public BuildView(BuildModel model) {
-        box = new Box(BoxLayout.Y_AXIS);
+        box = new Box(BoxLayout.X_AXIS);
         board = new BuildBoardView(model);
         buttonsPnl = new JPanel();
+        buttonsPnl.setLayout(new BoxLayout(buttonsPnl, BoxLayout.Y_AXIS));
         ButtonGroup bg = new ButtonGroup();
         JRadioButton moveBtn = new JRadioButton();
         moveBtn.setText("Move");
@@ -34,6 +35,36 @@ public class BuildView extends GameView {
                 "Left Flipper", "Right Flipper", "Ball", "Absorber"};
         gizmoList = new JComboBox<String>(gizmoTypes);
         gizmoList.setEnabled(false);
+
+        JPanel gravityPnl = new JPanel();
+        JLabel gravityLbl = new JLabel("Gravity: ");
+        JTextField gravityTxt = new JTextField();
+        gravityTxt.setText(String.valueOf(model.getGravity()));
+        //
+        // add listener to observer change of text
+        //
+        JPanel frictionMUPnl = new JPanel();
+        JLabel frictionMULbl = new JLabel("Friction MU: ");
+        JTextField frictionMUTxt = new JTextField();
+        frictionMUTxt.setText(String.valueOf(model.getFrictionMu()));
+        //
+        // add listener to observer change of text
+        //
+        JPanel frictionMU2Pnl = new JPanel();
+        JLabel frictionMU2Lbl = new JLabel("Friction MU2: ");
+        JTextField frictionMU2Txt = new JTextField();
+        frictionMU2Txt.setText(String.valueOf(model.getFrictionMu2()));
+        //
+        // add listener to observer change of text
+        //
+
+        gravityPnl.add(gravityLbl);
+        gravityPnl.add(gravityTxt);
+        frictionMUPnl.add(frictionMULbl);
+        frictionMUPnl.add(frictionMUTxt);
+        frictionMU2Pnl.add(frictionMU2Lbl);
+        frictionMU2Pnl.add(frictionMU2Txt);
+
         bg.add(moveBtn);
         bg.add(deleteBtn);
         bg.add(rotateBtn);
@@ -46,6 +77,10 @@ public class BuildView extends GameView {
         buttonsPnl.add(connectBtn);
         buttonsPnl.add(addBtn);
         buttonsPnl.add(gizmoList);
+        buttonsPnl.add(gravityPnl);
+        buttonsPnl.add(frictionMUPnl);
+        buttonsPnl.add(frictionMU2Pnl);
+        buttonsPnl.add(Box.createGlue());
         box.add(board);
         box.add(buttonsPnl);
     }
