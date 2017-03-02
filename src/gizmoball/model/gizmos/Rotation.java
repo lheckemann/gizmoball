6b0ec331147c1e41addfd2b7c3c8dced5e53b510
Linus Heckemann <git@sphalerite.org>
2017-03-02 09:57:15 +0000

    Move Rotation changes into Rotation enum

--- src/gizmoball/model/gizmos/Rotation.java
+++ src/gizmoball/model/gizmos/Rotation.java
@@ -3,7 +3,7 @@ package gizmoball.model.gizmos;
 public enum Rotation {
     NORTH(0), EAST(1), SOUTH(2), WEST(3);
 
-    private int turns;
+    private final int turns;
 
     Rotation(int turns) {
         this.turns = turns;
@@ -15,5 +15,14 @@ public enum Rotation {
 
     public double getRadiansFromNorth() {
         return Math.PI / 2 * turns;
+    }
+
+    public Rotation nextCW() {
+        switch(this) {
+            case NORTH: return EAST;
+            case EAST: return SOUTH;
+            case SOUTH: return WEST;
+            case WEST: return NORTH;
+        }
     }
 }
