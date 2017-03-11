7d60053cda55273fce46d1efca2f4d3487b63e3f
Linus Heckemann <git@sphalerite.org>
2017-03-11 08:53:27 +0000

    Nicer visuals for spawner and sink

--- src/gizmoball/view/elements/SinkView.java
+++ src/gizmoball/view/elements/SinkView.java
@@ -3,14 +3,17 @@ package gizmoball.view.elements;
 import gizmoball.model.gizmos.ReadGizmo;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 
 public class SinkView {
     public static void paint(Graphics2D g, ReadGizmo gizmo) {
-        g.setColor(Color.MAGENTA);
-        g.drawOval(0, 0, 1, 1);
         g.setColor(Color.red);
-        g.draw(new Line2D.Double(0.5, 0, 0.5, 1));
-        g.draw(new Line2D.Double(0, 0.5, 1, 0.5));
+        g.draw(new Ellipse2D.Double(0, 0, 1, 1));
+        g.draw(new Ellipse2D.Double(0.25, 0.25, 1.0/2, 1.0/2));
+        g.draw(new Line2D.Double(0.5, 0, 0.5, 0.25));
+        g.draw(new Line2D.Double(0, 0.5, 0.25, 0.5));
+        g.draw(new Line2D.Double(0.5, 1, 0.5, 0.75));
+        g.draw(new Line2D.Double(1, 0.5, 0.75, 0.5));
     }
 }
--- src/gizmoball/view/elements/SpawnerView.java
+++ src/gizmoball/view/elements/SpawnerView.java
@@ -8,7 +8,8 @@ import java.awt.geom.Ellipse2D;
 public class SpawnerView {
 
     public static void paint(Graphics2D g, ReadGizmo gizmo) {
-        g.setColor(Color.gray);
-        g.fill(new Ellipse2D.Double(0, 0, 1, 1));
+        g.setColor(Color.green);
+        g.draw(new Ellipse2D.Double(0, 0, 1, 1));
+        g.draw(new Ellipse2D.Double(0.25, 0.25, 1.0/2, 1.0/2));
     }
 }
