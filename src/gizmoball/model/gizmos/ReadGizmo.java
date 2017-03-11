2d901c81626cc56db7644b9cd9652f5677903245
Linus Heckemann <git@sphalerite.org>
2017-03-11 14:45:41 +0000

    Simplify Saver

--- src/gizmoball/model/gizmos/ReadGizmo.java
+++ src/gizmoball/model/gizmos/ReadGizmo.java
@@ -1,6 +1,9 @@
 package gizmoball.model.gizmos;
 
 import java.awt.geom.AffineTransform;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public interface ReadGizmo {
 
@@ -11,6 +14,32 @@ public interface ReadGizmo {
 
         public String toString() {
             return super.toString().replace("_", " ").toLowerCase();
+        }
+
+        public String saveName() {
+            boolean uppercaseNext = true;
+            StringBuilder out = new StringBuilder();
+            for (char c : super.toString().toCharArray()) {
+                if (c == '_') {
+                    uppercaseNext = true;
+                    continue;
+                }
+                if (uppercaseNext) {
+                    out.append(Character.toUpperCase(c));
+                    uppercaseNext = false;
+                } else {
+                    out.append(Character.toLowerCase(c));
+                }
+            }
+            return out.toString();
+        }
+
+        private static final Set<GizmoType> specified = Stream.of(
+                ABSORBER, SQUARE, CIRCLE, TRIANGLE, RIGHT_FLIPPER, LEFT_FLIPPER
+        ).collect(Collectors.toSet());
+
+        public boolean isSpecified() {
+            return specified.contains(this);
         }
     }
 
