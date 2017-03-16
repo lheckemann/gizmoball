4e8a6bd0399a240b8d666ed0468a155d43d746fa
William Macdonald <williamacdonald18@googlemail.com>
2017-03-16 15:51:37 +0000

    Fix connect gizmos message

--- src/gizmoball/controller/SwitchToConnectGizmosListener.java
+++ src/gizmoball/controller/SwitchToConnectGizmosListener.java
@@ -22,7 +22,7 @@ public class SwitchToConnectGizmosListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         this.board.switchToConnectGizmos(view, model);
-        this.view.setDisplayLabel("Click on the gizmo you wish to send the trigger and then click the gizmo you want to react to the trigger");
+        this.view.setDisplayLabel("Click on the gizmo you want to trigger, then click on the gizmo you want to trigger it");
     }
 
 
